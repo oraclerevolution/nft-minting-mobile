5224e9b82430ba63858e36d17ba2d6381aeb7d51
oracle_dev <a.ngoran@panelyscash.com>
2022-09-23 13:21:28 +0000

    formulaire de transfert de token

--- pages/SendToken.js
+++ pages/SendToken.js
@@ -1,14 +1,39 @@
 import React, {useState} from 'react'
-import {Text, StyleSheet, View} from "react-native"
+import {Text, StyleSheet, View, Alert} from "react-native"
 import {TextInput, Button} from "react-native-paper";
 
 const SendToken = ({navigation}) => {
     const [address, setAddress] = useState('')
-    const [address_sender, setNom] = useState('')
+    const [address_sender, setAddress_sender] = useState('')
     const [montant, setMontant] = useState('')
     return(
         <View style={styles.container}>
             <Text style={styles.text}>Envoyer un NFT</Text>
+            <TextInput
+                style={{margin:"4%", height:55}}
+                label="Adresse de l'envoyeur"
+                placeholder='0x....'
+                value={address_sender}
+                onChangeText={text => setAddress_sender(text)}
+            />
+            <TextInput
+                style={{margin:"4%", height:55}}
+                label="Quantité à envoyer"
+                placeholder='10'
+                keyboardType="number-pad"
+                value={montant}
+                onChangeText={text => setMontant(text)}
+            />
+            <TextInput
+                style={{margin:"4%", height:55}}
+                label="Adresse du recepteur"
+                placeholder='0x....'
+                value={address}
+                onChangeText={text => setAddress(text)}
+            />
+
+            <Button mode="contained" icon="send" style={{width:250, alignSelf:"center"}} onPress={()=>Alert.alert("Désolé", "Page en clours de développement")}>Envoyer le NFT</Button>
+
         </View>
     )
 }
@@ -16,8 +41,8 @@ const SendToken = ({navigation}) => {
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems:"center",
-        justifyContent:"center"
+        justifyContent:"center",
+        padding:15
     },
     text:{
         fontSize:18,
