7ad9aaf8ce3e0b8e491ca24f7788c096ba5c76b2
oracle_dev <a.ngoran@panelyscash.com>
2022-09-23 13:03:40 +0000

    racine app.js

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+
+import Mint from "./pages/Mint"
+import TakePicture from "./pages/TakePicture";
+import Accueil from './pages/Accueil'
+import sendToken from "./pages/SendToken";
+
+const MyStack = createStackNavigator()
+
+    function Stack() {
+      return (
+          <MyStack.Navigator initialRouteName="Accueil" screenOptions={{headerShown: false}} >
+                <MyStack.Screen name="Mint" component={Mint} />
+                <MyStack.Screen name="Accueil" component={Accueil} />
+                <MyStack.Screen name="TakePicture" component={TakePicture} />
+                <MyStack.Screen name="sendToken" component={sendToken} />
+          </MyStack.Navigator>
+      );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack />
+      </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
