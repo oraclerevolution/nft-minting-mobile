0355f48732af4448eda8db4424dbc70e2b0fdb4b
oracle_dev <a.ngoran@panelyscash.com>
2022-09-23 12:38:40 +0000

    connexion a pinnata

---
+++ helpers/pinata.js
@@ -0,0 +1,29 @@
+const key = "35386f5dfb82e9b9988d"
+const secret = "c6737452e55a5d39226615645d202efc4bcadd9c4a08d6c049494574158df9bb"
+
+const axios = require('axios')
+
+export const pinJSONToIPFS = async(JSONBody) => {
+    const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+    //making axios POST request to Pinata ⬇️
+    return axios
+        .post(url, JSONBody, {
+            headers: {
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
+            }
+        })
+        .then(function (response) {
+            return {
+                success: true,
+                pinataUrl: "https://gateway.pinata.cloud/ipfs/" + response.data.IpfsHash
+            };
+        })
+        .catch(function (error) {
+            console.log(error)
+            return {
+                success: false,
+                message: error.message,
+            }
+        });
+}
